f5c77bda8a273e8021a03261b58d5e538a8636e8
FacundoLG <facu-judoca@hotmail.com.ar>
2021-06-26 10:37:24 -0300

    basic sistem works

--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-
-const CartCard = () => {
+import '../styles/components/CartCard.css'
+const CartCard = (props) => {
+    const {data} = props
     return (
-        <div>
-            
+        <div className="CartData">
+            <div className="img" style={{backgroundImage: `url(${data.images})`}}>
+
+            </div>
+            <div className="cartInfo">
+                <h4>{data.name}</h4>
+                <h4>X1</h4>
+            </div>
         </div>
     )
 }
-
 export default CartCard
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/components/Nav.css'
 import {BiCartAlt} from 'react-icons/bi'
-const Nav = () => {
+import { connect } from 'react-redux'
+import CartCard from './CartCard'
+const Nav = (props) => {
+    const {data} = props
+    const [cart,setCart] = useState(false)
+
+    const handleCart = () => {
+        setCart(!cart)
+    }
     return (
         <>
         <div className="Nav">
                 <h2>TestStore</h2>
             <div>
-                <BiCartAlt className="icon"/>
+                <BiCartAlt className="icon" onClick={handleCart} />
             </div>
         </div>
+        {cart ?
         <div children="cart">
             <div className="cartList">
+                {
+                   data.length > 0?
+                   data.map((product) =>
+                   <CartCard data={product}/>
+                   )
+                   :
+                   <p>Nothing in the cart</p>
+                }
             </div>
-        </div>
+        </div>:""
+        }
         </>
     )
 }
 
-export default Nav
+const MapStateToProps = (state) => ({
+    data: state.Cart
+})
+export default connect(MapStateToProps,null)(Nav)
